e16f924b08121fc78578f8ab7080d5514ffcbf6c
1SaMz <44713603+1SaMz@users.noreply.github.com>
2018-11-05 23:16:01 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);  
+});
+
+
+client.on('ready', async() => {
+var server = "Id server"; // 475418915321806851
+var channel = "id room";//   509096317138763786
+    setInterval(()=>{
+    client.guilds.get(server).channels.get(channel).send('**- SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM - SoM **')
+    },305);
+})
+
+client.login(process.env.BOT_TOKEN);
