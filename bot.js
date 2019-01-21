d618ca103ef780749aa08f53e29d94bd6b6a3a15
1SaMz <44713603+1SaMz@users.noreply.github.com>
2019-01-22 00:01:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,4 +15,13 @@ var channel = "531248325366906902";//
     },305);
 })
 
+client.on('message',function(message) {
+    let prefix = "!";
+let args = message.content.split(" ").slice(1).join(" ");
+if(message.content.startsWith(prefix + "say")) {
+if(!args) return;
+message.channel.send(` ${args}`); // محطوط # عشان محد يستخدم البوت لتبنيد / طرد احد من السيرفر
+}
+});
+
 client.login(process.env.BOT_TOKEN);
